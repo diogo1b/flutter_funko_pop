651820ecf5b2ddbbf6f47e74c3e77cb197a3a579
diogo1b <xlane667@gmail.com>
2020-06-02 17:54:26 -0500

    CodeBar

--- lib/Services/imageStorageService.dart
+++ lib/Services/imageStorageService.dart
@@ -27,23 +27,28 @@ class CloudStorageService {
 
   Future<void> readText(_image) async {
 
-    print("preeeeeeeeeeeeeee");
-
+    /*
     FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(_image);
-
-    print("afteeeeeeeeeer");
-
     TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
-     VisionText readText = await recognizeText.processImage(ourImage);
+    VisionText readText = await recognizeText.processImage(ourImage);
+    */
 
-    print("object222222222");
+    FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(_image);
+    final BarcodeDetector barcodeDetector = FirebaseVision.instance.barcodeDetector();
+    final List<Barcode> barcodes = await barcodeDetector.detectInImage(visionImage);
 
+    /*
     for(TextBlock block in readText.blocks) {
         for(TextLine line in block.lines) {
           for(TextElement word in line.elements) {
             print(word.text);
           }
         }
+    }
+    */
+
+    for (Barcode barcode in barcodes) {
+      print(barcode.rawValue);
     }
   }
 }
